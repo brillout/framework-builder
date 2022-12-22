a10b7ebd98c11dfd4efb8bf150017037656cb9e8
Romuald Brillout <git@brillout.com>
2022-12-22 14:23:44 +0100

    restore relative path of assets imported from CSS

--- src/fixAssetsDir.ts
+++ src/fixAssetsDir.ts
@@ -19,20 +19,28 @@ async function fixAssetsDir() {
 
       if (!fileContent.includes(FRAMEWORK_BUILDER_ASSET_DIR)) return
 
-      // Prerend `/assets/` to non-JavaScript assets (images, fonts, ...)
-      assert(FRAMEWORK_BUILDER_ASSET_DIR.endsWith('/'))
-      fileContent = fileContent.split(`= "${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`= "/assets/`)
-      fileContent = fileContent.split(`url(${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`url(/assets/`)
-
-      // Restore JavaScript imports and don't prepend `/assets/`
-      let pathToDistRoot = path.relative(path.dirname(filePath), distDir)
-      if (pathToDistRoot === '') {
-        pathToDistRoot = '.'
+      // Absolute imports with `/assets/` base
+      {
+        assert(FRAMEWORK_BUILDER_ASSET_DIR.endsWith('/'))
+        // Asset imports from JavaScript
+        fileContent = fileContent.split(`= "${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`= "/assets/`)
       }
-      assert(pathToDistRoot.startsWith('.') && !pathToDistRoot.endsWith('/'), pathToDistRoot)
-      fileContent = fileContent.split(`from "${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`from "${pathToDistRoot}/`)
-      fileContent = fileContent.split(`import "${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`import "${pathToDistRoot}/`)
-      fileContent = fileContent.split(`import("${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`import("${pathToDistRoot}/`)
+
+      // Relative imports
+      {
+        let pathToDistRoot = path.relative(path.dirname(filePath), distDir)
+        if (pathToDistRoot === '') {
+          pathToDistRoot = '.'
+        }
+        assert(pathToDistRoot.startsWith('.') && !pathToDistRoot.endsWith('/'), pathToDistRoot)
+        // JavaScript imports
+        fileContent = fileContent.split(`from "${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`from "${pathToDistRoot}/`)
+        fileContent = fileContent.split(`import "${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`import "${pathToDistRoot}/`)
+        fileContent = fileContent.split(`import("${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`import("${pathToDistRoot}/`)
+        // Asset imports from CSS
+        fileContent = fileContent.split(`url(${FRAMEWORK_BUILDER_ASSET_DIR}`).join(`url(${pathToDistRoot}/`)
+      }
+
       assert(!fileContent.includes(FRAMEWORK_BUILDER_ASSET_DIR))
 
       await fsp.writeFile(filePath, fileContent, 'utf8')
